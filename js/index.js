b81dd9dc48d0b6ea5af06afb09e0873e9de628e9
tgwuyangyang <375959344@qq.com>
2016-12-07 14:43:49 +0800

    时间控件

--- js/index.js
+++ js/index.js
@@ -11,7 +11,8 @@ var java = [{"id": "1", "name": "RESTful架构", "url": "html/java/rest.html", "
            ];
 var AngularJS = [{"id": "1", "name": "angularjs学习笔记--iphone上时间转换NaN问题", "url": "html/AngularJS/date-format.html", "time": "2016-08-02"},
                  {"id": "2", "name": "angularjs学习笔记--js实现打电话", "url": "html/AngularJS/phone.html", "time": "2016-08-03"},
-                 {"id": "3", "name": "angularjs学习笔记--SPA单页面应用页面跳转类似于微信的动画效果", "url": "html/AngularJS/skip_cartoon.html", "time": "2016-09-21"}
+                 {"id": "3", "name": "angularjs学习笔记--SPA单页面应用页面跳转类似于微信的动画效果", "url": "html/AngularJS/skip_cartoon.html", "time": "2016-09-21"},
+                 {"id": "4", "name": "angularjs学习笔记--directive封装mui时间控件", "url": "html/AngularJS/btn_date.html", "time": "2016-12-07"}
                 ];
 var github = [{"id": "1", "name": "git常用命令", "url": "html/github/command.html", "time": "2016-07-31"}];
 var mui = [{"id": "1", "name": "mui使用小技巧", "url": "html/mui/notes.html", "time": "2016-08-26"}];
