395219421387b7bb9d8c92def237e4c6fa1af0fe
tgwuyangyang <375959344@qq.com>
2018-09-19 16:13:28 +0800

    update namae

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 var java = [{ "id": "1", "name": "RESTful架构", "url": "html/java/rest.html", "time": "2016-07-31" },
    { "id": "2", "name": "word、excel、pdf文件的加密与解密", "url": "html/java/confidentiality.html", "time": "2016-09-23" }
 ];
-var Angular1 = [{ "id": "1", "name": "iphone上时间转换NaN问题", "url": "html/Angular1/date-format.html", "time": "2016-08-02" },
-   { "id": "2", "name": "js实现打电话", "url": "html/Angular1/phone.html", "time": "2016-08-03" },
-   { "id": "3", "name": "SPA单页面应用页面跳转类似于微信的动画效果", "url": "html/Angular1/skip_cartoon.html", "time": "2016-09-21" },
-   { "id": "4", "name": "directive封装mui时间控件", "url": "html/Angular1/btn_date.html", "time": "2016-12-07" },
-   { "id": "8", "name": "mui与angular1实现轮播刷新数据", "url": "html/Angular1/carousel.html", "time": "2017-01-11" },
-   { "id": "5", "name": "directive封装d3.js", "url": "html/Angular1/d3.html", "time": "2017-01-14" },
-   { "id": "6", "name": "directive封装输入框获取焦点和失去焦点", "url": "html/Angular1/focus_blur.html", "time": "2017-02-13" },
-   { "id": "7", "name": "屏幕向上滑动，顶部菜单隐藏，屏幕向下滑动，顶部菜单显示", "url": "html/Angular1/scroll_up_down.html", "time": "2017-02-23" },
-   { "id": "8", "name": "ng-change", "url": "html/Angular1/ng_change.html", "time": "2017-05-13" },
-   { "id": "9", "name": "AngularJs删除历史浏览记录", "url": "html/Angular1/2018.9.19.html", "time": "2018-09-19" }
+var AngularJs = [{ "id": "1", "name": "iphone上时间转换NaN问题", "url": "html/AngularJs/date-format.html", "time": "2016-08-02" },
+   { "id": "2", "name": "js实现打电话", "url": "html/AngularJs/phone.html", "time": "2016-08-03" },
+   { "id": "3", "name": "SPA单页面应用页面跳转类似于微信的动画效果", "url": "html/AngularJs/skip_cartoon.html", "time": "2016-09-21" },
+   { "id": "4", "name": "directive封装mui时间控件", "url": "html/AngularJs/btn_date.html", "time": "2016-12-07" },
+   { "id": "8", "name": "mui与AngularJs实现轮播刷新数据", "url": "html/AngularJs/carousel.html", "time": "2017-01-11" },
+   { "id": "5", "name": "directive封装d3.js", "url": "html/AngularJs/d3.html", "time": "2017-01-14" },
+   { "id": "6", "name": "directive封装输入框获取焦点和失去焦点", "url": "html/AngularJs/focus_blur.html", "time": "2017-02-13" },
+   { "id": "7", "name": "屏幕向上滑动，顶部菜单隐藏，屏幕向下滑动，顶部菜单显示", "url": "html/AngularJs/scroll_up_down.html", "time": "2017-02-23" },
+   { "id": "8", "name": "ng-change", "url": "html/AngularJs/ng_change.html", "time": "2017-05-13" },
+   { "id": "9", "name": "AngularJs删除历史浏览记录", "url": "html/AngularJs/2018.9.19.html", "time": "2018-09-19" }
 ];
-var Angular2 = [{ "id": "1", "name": "angular2环境搭建", "url": "html/Angular2/setting.html", "time": "2017-02-10" }];
+var Angular = [{ "id": "1", "name": "angular2环境搭建", "url": "html/Angular/setting.html", "time": "2017-02-10" }];
 var github = [{ "id": "1", "name": "git常用命令", "url": "html/github/command.html", "time": "2016-07-31" },
    { "id": "2", "name": "使用github中的Issues给博客搭建评论功能", "url": "html/github/comment.html", "time": "2017-06-16" }];
 var mui = [{ "id": "1", "name": "mui使用小技巧", "url": "html/mui/notes.html", "time": "2016-08-26" },
@@ -33,8 +33,8 @@ var WeChat = [{ "id": "1", "name": "微信小程序的优点与缺点", "url": "
 var livelihood = [{ "id": "1", "name": "人性", "url": "html/livelihood/2017_03_18.html", "time": "2017-03-18" }];
 var other = [];
 var menu = [{ "id": "1", "name": "java", "data": java},
-   { "id": "2", "name": "Angular1", "data": Angular1},
-   { "id": "3", "name": "Angular2", "data": Angular2},
+   { "id": "2", "name": "AngularJs", "data": AngularJs},
+   { "id": "3", "name": "Angular", "data": Angular},
    { "id": "4", "name": "github", "data": github},
    { "id": "5", "name": "mui", "data": mui},
    { "id": "6", "name": "css", "data": css},
